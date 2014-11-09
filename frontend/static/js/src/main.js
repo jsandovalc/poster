7eeea4f1ffb8558b752a2561c54985d01789c1a4
Jonathan Sandoval <jsandoval@utp.edu.co>
2014-11-09 00:11:03 -0500

    Textinput to post. Pending a button.

--- frontend/static/js/src/main.js
+++ frontend/static/js/src/main.js
@@ -1,22 +1,23 @@
 /*global famous*/
 // import dependencies
 var Engine = famous.core.Engine;
+var TextAreaSurface = famous.surfaces.TextareaSurface;
 var Surface = famous.core.Surface;
+var Transform = famous.core.Transform;
+var StateModifier = famous.modifiers.StateModifier;
 
 var mainContext = Engine.createContext();
 
-var firstSurface = new Surface({
-    content: "<h3>Hi!</h3>",
-    size: [200, 200],
-    properties: {
-        backgroundColor: 'rgb(240, 238, 233)',
-        textAlign: 'center',
-        padding: '5px',
-        border: '2px solid rgb(210, 208, 203)',
-        marginTop: '50px',
-        marginLeft: '50px'
-    }
+var textAreaSurface = new TextAreaSurface({
+    size: [200, 100]
+});
+
+textAreaSurface.setPlaceholder('Post it!');
+
+var centerModifier = new StateModifier({
+    align: [0.5, 0.5],
+    origin: [0.5, 0.5]
 });
 
 
-mainContext.add(firstSurface);
+mainContext.add(centerModifier).add(textAreaSurface);
