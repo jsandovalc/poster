5d8fd22cd65f674ba1f2d7cad05609be36a481bc
Jonathan Sandoval Cardona <ark@debian>
2019-04-18 20:54:31 -0500

    Try to fix poster project.

---
+++ poster/users/tests/test_db_utils.py
@@ -0,0 +1,13 @@
+import pytest
+
+from pytest import fixture
+
+from poster.users.db_utils import select_user_by_id
+
+
+@pytest.mark.asyncio
+async def test_select_user(tables: fixture, sa_engine: fixture) -> None:
+    async with sa_engine.acquire() as conn:
+        res = await select_user_by_id(conn, 1)
+
+    assert res.id == 1
